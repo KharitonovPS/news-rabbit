1c792bc9523f978be26fcef803fa45ea0d159b9c
KharitonovPS <pasyaoo@gmail.com>
2023-12-27 14:42:32 +0300

    Initial commit

---
+++ src/main/java/org/kharitonov/messaging/config/RabbitMQConfig.java
@@ -0,0 +1,57 @@
+package org.kharitonov.messaging.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.queue.json.name}")
+    private String jsonQueue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Value("${rabbitmq.routing.json.key}")
+    private String routingJsonKey;
+
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+
+    @Bean
+    public Queue jsonQueue(){
+        return  new Queue(jsonQueue);
+    }
+
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    @Bean
+    public Binding jsonBinding(){
+        return BindingBuilder.bind(jsonQueue())
+                .to(exchange())
+                .with(routingJsonKey);
+    }
+}
